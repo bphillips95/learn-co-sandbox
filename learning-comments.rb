c61f51406fcf1344c2d94236dd6c112777d5055c
Baruch Phillips <baruchphillips@gmail.com>
2019-11-19 21:04:08 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,14 @@
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside 
+#   puts"code inside" 
+# e
+# puts"Code after if...end"
+
+count = 0 
+n = 3
+loop do
+  break if count >= n 
+  puts "I ran" 
+  count +=1 
+end
